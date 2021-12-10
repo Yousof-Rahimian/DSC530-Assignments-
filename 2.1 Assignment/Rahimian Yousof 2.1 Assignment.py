8f8fc3ba40421ab93ba52bd57661c5b0ca2cf185
YOUSOF RAHIMIAN <85660733+Yousof-Rahimian@users.noreply.github.com>
2021-12-10 11:40:09 -0800

    Add files via upload

---
+++ 2.1 Assignment/Rahimian Yousof 2.1 Assignment.py
@@ -0,0 +1,50 @@
+"""
+DSC530
+Week 2.1
+Preparing for Exploratory Data Analysis
+Author: Yousof Rahimian
+Instructor: Professor Shankar Parajulee
+12/10/2021
+"""
+# Display the Hello Word
+print('Hello Word')
+
+# Add two numbers together
+x= 3+34
+print(x)
+
+# Subtract a number from another number
+x= 20-5
+print(x)
+
+# Multiply two numbers
+x= 5*8
+print(x)
+
+# Divide between two numbers
+x= 30/5
+print(x)
+
+# Concatenate two strings together (any words)
+s1="Yousof"
+s2="Rahimian"
+s3=s1+s2
+s4=s1+" "+s2
+print(s3)
+print(s4)
+
+# Create a list of 4 items (can be strings, numbers, both)
+Names = ["Yousof", "Mike", "Joel", "Kyle"]
+Age = [35, 38, 54, 30]
+print(Names)
+print(Age)
+
+# Append an item to your list (again, can be a string, number)
+x = [[1, 2], [3, 4], [5, 6]]
+for sublist in x:
+    sublist.append('a')
+print(x)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = ("red", "blue", "green", "black")
+print(my_tuple)
